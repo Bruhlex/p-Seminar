29fa46ed5d250ae32352e2e1d476e6c0b9263f0e
Alex <imabloe@gmail.com>
2021-10-11 02:49:56 +0200

    asdasd

--- static/js/controller/satellit_zugriff.js
+++ static/js/controller/satellit_zugriff.js
@@ -1,7 +1,6 @@
 const POP_UP_BIG = document.querySelector(".pop_up-big")
 const TYPE       = "satellit_zugriff"
 
-
 localStorage.setItem("current", "c2F0ZWxsaXRfenVncmlmZg==")
 
 function showAssignment() {
--- static/js/controller/verbindung_zum_satelliten.js
+++ static/js/controller/verbindung_zum_satelliten.js
@@ -1,1 +1,11 @@
 const TYPE = "verbindung_zum_satelliten"
+
+var MUTEX = false
+
+function playAudio() {
+    if(MUTEX) return
+
+    MUTEX = true
+    const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
+    AUDIO.play();
+}
