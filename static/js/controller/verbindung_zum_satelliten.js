3aedea537687538a5df72db5739ec6685f5e8cbb
Alex <imabloe@gmail.com>
2021-11-18 21:23:49 +0100

    - fix

--- static/js/controller/verbindung_zum_satelliten.js
+++ static/js/controller/verbindung_zum_satelliten.js
@@ -1,5 +1,9 @@
 const TYPE = "verbindung_zum_satelliten"
 const AudioButton = document.querySelector("#audio_button")
+const PlayAudio = document.querySelector("#play_audio")
+const soundEffect = new Audio();
+soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+soundEffect.src = '../../assets/sounds/GesprachGPOHacker_2.mp3';
 
 var MUTEX = false
 
@@ -7,23 +11,33 @@ function playAudio() {
     if(MUTEX) return
 
     MUTEX = true
-
-    const soundEffect = new Audio();
-    soundEffect.autoplay = true;
-
     // onClick of first interaction on page before I need the sounds
     // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
-    soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
 
     // later on when you actually want to play a sound at any point without user interaction
-    soundEffect.src = '../../assets/sounds/GesprachGPOHacker_2.mp3';
     // const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
-    // AUDIO.play();
+    AUDIO.play();
     toggleElement(backscreen)
     toggleElement(AudioButton)
 }
+
+
 
 window.onload = function () {
     toggleElement(backscreen)
     toggleElement()
 }
+
+PlayAudio.addEventListener("click", function(){
+    if(MUTEX) return
+
+    MUTEX = true
+    // onClick of first interaction on page before I need the sounds
+    // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+
+    // later on when you actually want to play a sound at any point without user interaction
+    // const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
+    AUDIO.play();
+    toggleElement(backscreen)
+    toggleElement(AudioButton)
+} )
