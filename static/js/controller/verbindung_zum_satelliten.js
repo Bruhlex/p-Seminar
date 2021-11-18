91c8d26a1e1a3c8cd4d223caac60af18c3fc2ece
Alex <imabloe@gmail.com>
2021-11-18 21:16:10 +0100

    - Audio fix?

--- static/js/controller/verbindung_zum_satelliten.js
+++ static/js/controller/verbindung_zum_satelliten.js
@@ -7,13 +7,18 @@ function playAudio() {
     if(MUTEX) return
 
     MUTEX = true
+
+    const soundEffect = new Audio();
+    soundEffect.autoplay = true;
+
+    // onClick of first interaction on page before I need the sounds
+    // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+    soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+
+    // later on when you actually want to play a sound at any point without user interaction
+    soundEffect.src = '../../assets/sounds/GesprachGPOHacker_2.mp3';
     // const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
     // AUDIO.play();
-
-    var sound = new Howl({
-        src: ['../../assets/sounds/GesprachGPOHacker_2.mp3']
-      });
-    sound.play();
     toggleElement(backscreen)
     toggleElement(AudioButton)
 }
