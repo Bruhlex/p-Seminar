8c0567e7f98bb93d62e9042a7b417e6aa8d9a4b3
Alex <imabloe@gmail.com>
2021-11-18 19:39:23 +0100

    - test

--- static/js/controller/start.js
+++ static/js/controller/start.js
@@ -21,17 +21,17 @@ function loadContent(type) {
     let string = ""
     toggleElement(POP_UP_BIG)
     switch(type) {
-        case "start_a":
+        case "start_b":
             tip_type = "1"
-            string = `Der Wert dieser Variable befindet sich nicht hier... <br> Es ist jedoch bekannt, <br> dass der Hacker gerne mit Strecken zu bestimmten Orten rätselt. <br> Suche nach einer ganzen Strecke und nimm die Quersumme der gerundeten ganzen Zahl als "a". `
+            string = `Der Wert dieser Variable befindet sich nicht hier... <br> Es ist jedoch bekannt, <br> dass der Hacker gerne mit Strecken zu bestimmten Orten rätselt. <br> Suche nach einer ganzen Strecke und nimm die Quersumme der gerundeten ganzen Zahl als "b". `
             break
         case "start_c":
             tip_type = "3"
             string = `&#945;`
             break
-        case "start_b":
+        case "start_a":
             tip_type = "2"
-            string = `Auch dieser Wert befindet sich nicht hier.  <br> Jedoch hat die Variable etwas mit den momentan infizierten PC's zu tun. <br> Suche nach dieser Zahl und <br> nimm die Quersumme der gerundeten ganzen Zahl für "b". <br> Es wird nach einer ganzen Strecke zu dem Ort gesucht`
+            string = `Auch dieser Wert befindet sich nicht hier.  <br> Jedoch hat die Variable etwas mit den momentan infizierten PC's zu tun. <br> Suche nach dieser Zahl und <br> nimm die Quersumme der gerundeten ganzen Zahl für "a". <br> Es wird nach einer ganzen Strecke zu dem Ort gesucht`
             break
         default:
     }
