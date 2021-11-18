c3000b50d0d323de2ee071fcfb54e6d24cedbd41
Alex <imabloe@gmail.com>
2021-11-18 21:35:43 +0100

    - fix?

--- static/js/controller/verbindung_zum_satelliten.js
+++ static/js/controller/verbindung_zum_satelliten.js
@@ -1,8 +1,7 @@
 const TYPE = "verbindung_zum_satelliten"
 const AudioButton = document.querySelector("#audio_button")
 const PlayAudio = document.querySelector("#play_audio")
-const soundEffect = new Audio();
-soundEffect.src = '../../assets/sounds/GesprachGPOHacker_2.mp3';
+
 
 var MUTEX = false
 
@@ -10,6 +9,7 @@ function playAudio() {
     if(MUTEX) return
 
     MUTEX = true
+    const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
     // onClick of first interaction on page before I need the sounds
     // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
 
@@ -27,16 +27,4 @@ window.onload = function () {
     toggleElement()
 }
 
-PlayAudio.addEventListener("click", function(){
-    if(MUTEX) return
-
-    MUTEX = true
-    // onClick of first interaction on page before I need the sounds
-    // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
-
-    // later on when you actually want to play a sound at any point without user interaction
-    // const AUDIO = new Audio('../../assets/sounds/GesprachGPOHacker_2.mp3');
-    soundEffect.play();
-    toggleElement(backscreen)
-    toggleElement(AudioButton)
-} )
+PlayAudio.addEventListener("click", playAudio)
